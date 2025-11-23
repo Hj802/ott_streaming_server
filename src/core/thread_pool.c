1865d8b07c5837a221da349dcd0609529b0444ae
Hj802 <gmlwo1962@naver.com>
2025-11-23 21:46:31 +0900

    worker_thread_func(), thread_pool_submit()

--- src/core/thread_pool.c
+++ src/core/thread_pool.c
@@ -6,6 +6,10 @@
 
 static void* worker_thread_func(void* arg); // 워커 스레드가 실행할 함수
 
+typedef struct {
+    ThreadPool* pool;
+    int idx;
+} WorkerArg;
 
 int thread_pool_init(ThreadPool* pool, int num_threads, int queue_capacity){
     // 유효성 검사
@@ -30,9 +34,10 @@ int thread_pool_init(ThreadPool* pool, int num_threads, int queue_capacity){
     //  워커 스레드 생성 루프
     for (int i = 0; i < num_threads; ++i) {
         // arg로 pool 자체를 넘겨줍니다 (워커가 큐에 접근해야 하니까)
-        if (pthread_create(&pool->threads[i], NULL, worker_thread_func, pool) != 0) {
-            perror("Failed to create worker thread");
-
+        
+        WorkerArg* arg = (WorkerArg*)malloc(sizeof(WorkerArg));
+        if(arg == NULL){
+            perror("Failed to allocate memory for worker arg");
             // [중요] 롤백 로직 (All or Nothing)
             // 5번째에서 실패했다면, 0~3번 스레드는 이미 살아서 돌아가고 있음.
             // 얘네들을 안전하게 종료시켜야 함.
@@ -48,15 +53,48 @@ int thread_pool_init(ThreadPool* pool, int num_threads, int queue_capacity){
             // 자원 정리
             free(pool->threads);
             task_queue_free(&pool->queue);
-            
+            return -1;
+        }
+        arg->pool = pool;
+        arg->idx = i;
+
+        if (pthread_create(&pool->threads[i], NULL, worker_thread_func, arg) != 0) {
+            perror("Failed to create worker thread");
+            // 롤백 로직 (All or Nothing)
+            task_queue_shutdown(&pool->queue);
+            for (int j = 0; j < i; ++j) {
+                pthread_join(pool->threads[j], NULL);
+            }
+            free(pool->threads);
+            task_queue_free(&pool->queue);
             return -1; // 실패 반환
         }// if
     }// for
     return 0; // 성공
 }
 
-int thread_pool_submit(ThreadPool* pool, void (*function)(void*), void* arg){
+static void* worker_thread_func(void* arg){
+    WorkerArg* args = (WorkerArg*)arg;
 
+    ThreadPool* pool = args->pool;
+    int idx = args->idx;
+    free(args);
+    printf("Worker-%d started.\n", idx);
+    while(true){
+        Task task = task_queue_dequeue(&pool->queue);
+        if(task.function == NULL && task.arg == NULL){
+            printf("Worker thread %lu stopping.\n", (unsigned long)pthread_self());
+            break;
+        }
+        task.function(task.arg);
+    }
+    return NULL;
+}
+
+int thread_pool_submit(ThreadPool* pool, void (*function)(void*), void* arg){
+    if (pool == NULL || function == NULL) return -1;
+    Task task = {.function = function, .arg = arg};
+    return task_queue_try_enqueue(&pool->queue, task);
 }
 
 void thread_pool_shutdown(ThreadPool* pool){
@@ -66,7 +104,6 @@ void thread_pool_shutdown(ThreadPool* pool){
 void thread_pool_wait(ThreadPool* pool){
 
 }
-
 
 void thread_pool_cleanup(ThreadPool* pool){
 
