1f01490bfdbe21e83a87557fd5942e30a2809a33
Hj802 <gmlwo1962@naver.com>
2025-11-29 18:00:18 +0900

    try_read_request()

--- src/app/http_handler.c
+++ src/app/http_handler.c
@@ -1,12 +1,76 @@
-// http_parser.c의 진화.        파싱 + 요청 처리
+#define _POSIX_C_SOURCE 200809L
 
-/*
-# 역할: 들어온 데이터가 HTTP인지 확인하고, 적절한 담당자에게 넘겨주는 '라우터(Router)'.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/errno.h>
+#include "app/http_handler.h"
+#include "app/stream_handler.h"
+#include "app/client_context.h"
 
-# 핵심 기능:
-- parse_request(buffer): 들어온 문자열을 분석해 GET /video.mp4 HTTP/1.1 같은 정보를 구조체로 만듦.
-- route_request(request):
-    URL이 /login이면 -> auth_handler 호출.
-    URL이 /watch이면 -> stream_handler 호출.
-    URL이 /이면 -> index.html 읽어서 전송.
-*/
+enum {
+    READ_BLOCK = -2,
+    READ_ERR = -1,
+    READ_EOF
+};
+
+static int try_read_request(ClientContext *ctx);
+static void parse_request(ClientContext *ctx);
+static void route_request(ClientContext *ctx, char *path, char *method);
+static void rearm_epoll(ClientContext *ctx);
+
+void handle_http_request(void *arg) {
+    ClientContext *ctx = (ClientContext*)arg;
+
+    int read_status = try_read_request(ctx);
+    if (read_status == READ_ERR) return;
+    if (read_status == READ_BLOCK) return;
+
+    parse_request(ctx);
+
+}
+
+
+static int try_read_request(ClientContext *ctx) {
+    int remaining = (sizeof(ctx->buffer) - 1) - ctx->buffer_len;
+
+    if (remaining <= 0){
+        fprintf(stderr, "Error: Request Header too large (Buffer Full)\n");
+        return READ_ERR;
+    }
+
+    char *ptr = ctx->buffer + ctx->buffer_len;
+
+    ssize_t received = recv(ctx->client_fd, ptr, remaining, 0);
+
+    if (received > 0){
+        ctx->buffer_len += received;
+        ctx->buffer[ctx->buffer_len] = '\0';
+        return received;
+    } 
+    else if (received == 0) {
+        // FIN
+        return READ_EOF;
+    } 
+    else {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            // 에러 X. 데이터 없음
+            return READ_BLOCK;
+        }
+        perror("recv() failed.");
+        return READ_ERR;
+    }
+}
+
+static void parse_request(ClientContext *ctx) {
+
+}
+
+static void route_request(ClientContext *ctx, char *path, char *method) {
+
+}
+
+static void rearm_epoll(ClientContext *ctx) {
+
+}
