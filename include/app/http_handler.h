579b352a99ed1cbd4a3cf7c6f5ceff4dc07f524c
Hj802 <gmlwo1962@naver.com>
2025-11-29 18:02:32 +0900

    함수 선언

---
+++ include/app/http_handler.h
@@ -0,0 +1,14 @@
+#ifndef HTTP_HANDLER_H
+#define HTTP_HANDLER_H
+
+
+/**
+ * @brief 워커 스레드가 수행할 HTTP 요청 처리 진입점
+ * * 1. TaskQueue에서 꺼낸 작업을 처리하는 메인 함수.
+ * 2. 인자로 전달된 Context를 캐스팅하여 사용.
+ * 3. 읽기 -> 파싱 -> 라우팅 -> 응답의 흐름을 지휘.
+ * * @param arg (ClientContext*) 타입으로 캐스팅될 인자
+ */
+void handle_http_request(void *arg);
+
+#endif
