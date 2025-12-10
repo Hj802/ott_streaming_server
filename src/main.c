39853decccee8cc6a5658d37bb13d74178b9aa65
Hj802 <gmlwo1962@naver.com>
2025-12-10 16:11:02 +0900

    시그널 추가, db 초기화, 자원 해제 및 정리

--- src/main.c
+++ src/main.c
@@ -1,19 +1,30 @@
 #define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
-// core Headers
 #include "core/reactor.h"
 #include "core/thread_pool.h"
 #include "core/config_loader.h"
-
-// app Headers
 #include "app/http_handler.h"
 #include "app/db_handler.h"
 
- #include <stdio.h>
- #include <stdlib.h>
+ // 시그널 핸들러용
+Reactor *g_reactor_ptr = NULL;
 
- int main() {
+void signal_handler(int sig) {
+    if (sig == SIGINT) {
+        printf("\nCaught SIGINT, stopping server...\n");
+        if (g_reactor_ptr) {
+            reactor_stop(g_reactor_ptr);
+        }
+    }
+}
+
+ int main(int argc, char *argv[]) {
     ServerConfig config;
+    signal(SIGPIPE, SIG_IGN); // 클라이언트가 연결을 끊었을 때 서버가 죽는 것을 방지
 
     printf("Loading configuration...\n");
     if (load_config("config/server.conf", &config) == -1) {
@@ -21,15 +32,44 @@
         return -1;
     }
 
-    ThreadPool pool = {0};
+    if (db_init("ott.db") != 0) {
+        fprintf(stderr, "Failed to initialize Database.\n");
+        return -1;
+    }
 
-    int num_thread = config.thread_num;
-    int queue_capacity = config.queue_capacity;
-
-    thread_pool_init(&pool, num_thread, queue_capacity);
+    ThreadPool pool = {0};;
+    if (thread_pool_init(&pool, config.thread_num, config.queue_capacity)) {
+        fprintf(stderr, "Failed to init thread pool.\n");
+        db_cleanup();
+        return -1;
+    }
 
     Reactor reactor = {0};
+    if (reactor_init(&reactor, &pool, &config) != 0) {
+        fprintf(stderr, "Failed to init reactor.\n");
+        thread_pool_shutdown(&pool);
+        thread_pool_wait(&pool);
+        thread_pool_cleanup(&pool);
+        db_cleanup();
+        return 1;
+    }
 
-    reactor_init(&reactor, &pool, &config);
+    g_reactor_ptr = &reactor;
+    signal(SIGINT, signal_handler);
+
+    printf("Server running on port %d...\n", config.port);
     reactor_run(&reactor);
+
+    printf("Cleaning up resources...\n");
+    
+    thread_pool_shutdown(&pool);
+    thread_pool_wait(&pool);
+    thread_pool_cleanup(&pool);
+    
+    reactor_destroy(&reactor);
+    
+    db_cleanup();
+
+    printf("Server stopped cleanly.\n");
+    return 0;
  }
